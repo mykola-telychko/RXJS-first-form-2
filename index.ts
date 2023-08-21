be27b70a81c38b0977997422c018994496a86e5a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-22 00:59:30 +0300

    fin

--- index.ts
+++ index.ts
@@ -4,10 +4,22 @@ import { first } from 'rxjs/operators';
 // https://www.learnrxjs.io/learn-rxjs/operators/filtering/first
 // Example 2: First value to pass predicate
 
-const source = from([1, 2, 3, 4, 5]);
-//emit first item to pass test
-const example = source.pipe(first((num) => num === 5));
-//output: "First to pass test: 5"
+// EXPL-1-begin //emit first item to pass test
+// const source = from([1, 2, 3, 4, 5]);
+// const example = source.pipe(first((num) => num === 5));
+// const subscribe = example.subscribe((val) =>
+//   console.log(`First to pass test: ${val}`)
+// );
+// EXPL-1-end //output: "First to pass test: 5"
+
+// EXPL-2-begin //emit first item to pass test
+const listOfArr = from([
+  [72, 3, 4, 5],
+  [2, 31, 54, 50],
+  [29, 44, 54, 5],
+]);
+const example = listOfArr.pipe(first((arr) => arr.includes(31)));
 const subscribe = example.subscribe((val) =>
   console.log(`First to pass test: ${val}`)
 );
+// EXPL-2-end //output: "First finding arr on condition"
